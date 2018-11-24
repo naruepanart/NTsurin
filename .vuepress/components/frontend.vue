2de08e5307c80f7c8ad880d703593cc57d7bdea5
Naruepanart Siangsanan <benzmaster2@hotmail.com>
2018-11-24 19:01:23 +0700

    stable

--- .vuepress/components/frontend.vue
+++ .vuepress/components/frontend.vue
@@ -11,7 +11,13 @@
     <p>เลขที่ : &nbsp;&nbsp;
     <input type="number" class="form-control input-md" placeholder="1" required="" v-model="numberclassroom" size="10" min="1"></p>
     <hr>
-    <p>หากจังหวัดสุรินทร์กำลังเร่งเดินหน้าพัฒนา เพื่อเข้าสู่ยุค "เมืองอัจฉริยะ (Smart City)" ควรจะพัฒนาด้านใดบ้าง ?</p>
+    <p>หากจังหวัดสุรินทร์กำลังเร่งเดินหน้าพัฒนา เพื่อเข้าสู่ยุค "เมืองอัจฉริยะ (Smart City)" ควรจะพัฒนาด้านใดบ้าง ? <b>(ให้เลือกตอบคนละ 1 ข้อ)</b></p>
+    <p>ยกตัวอย่าง</p>
+    <p>1.ด้านการขนส่ง</p>
+    <p>2.ด้านการจัดการขยะ</p>
+    <p>3.ด้านการเกษตร</p>
+    <p>4.ด้านการท่องเที่ยว</p>
+    <p>5.ด้านสุขภาพ</p>
     <p>เช่น ควรจะพัฒนาในด้านการท่องเที่ยว เพราะ...</p>
     <textarea cols="35" rows="20" v-model="text" placeholder="ควรจะพัฒนาในด้าน... เพราะ..." required=""></textarea><br>
     <a href="https://www.google.co.th/search?q=smart+city&rlz=1C1GCEA_enTH819TH819&oq=smart&aqs=chrome.2.69i57j69i61j69i59j69i61j0l2.5351j0j7&sourceid=chrome&ie=UTF-8">แหล่งค้นหาคำตอบ</a>
