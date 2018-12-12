441cbfa91994ed0da730985b03c711c7c8950ad3
Naruepanart <naruepanart1201@gmail.com>
2018-12-12 14:43:44 +0700

    Update resultmailfuture.vue

--- .vuepress/components/resultmailfuture.vue
+++ .vuepress/components/resultmailfuture.vue
@@ -4,32 +4,30 @@
     <p>จำนวนคนส่ง : {{fetchAPI.length}} คน</p>
     <hr>
     <div v-for="total, i in fetchAPI">
-      <p>{{total.titlename}} {{total.firstname}} {{total.lastname}} -
+      <p>{{total.titlename}} {{total.firstname}} {{total.lastname}}
+        &nbsp;
       {{total.classroom}} เลขที่ {{total.numberinclassroom}}
       </p>
       <p>
-        รายละเอียด :
-        <button class="red" v-on:click="onDelete(total._id,i)">Delete</button>
-        -
-        <button class="gray" v-on:click="editResult = total._id">Edit</button>&nbsp;
-        <button class="green" v-on:click="UpdateResult(total)">Update</button>
+        <button class="red" v-on:click="onDelete(total._id,i)">ลบ</button>
+        <button class="gray" v-on:click="editResult = total._id">แก้ไข</button>
+        <button class="green" v-on:click="UpdateResult(total)">อัพเดท</button>
         <br>
       </p>
-      <div v-if="editResult === total._id">คำนำหน้านาม :
+      <div v-if="editResult === total._id">คำนำหน้านาม 
         <input type="text" v-model="total.titlename">
-        <br>ชื่อ :
+        <br>ชื่อ 
         <input type="text" v-model="total.firstname">
-        <br>นามสกุล :
+        <br>นามสกุล 
         <input type="text" v-model="total.lastname">
-        <br>ห้อง :
+        <br>ห้อง 
         <input type="text" v-model="total.classroom">
-        <br>เลขที่ :
+        <br>เลขที่ 
         <input type="number" v-model="total.numberinclassroom">
       </div>
       <div v-else>
         <textarea rows="15">{{total.text}}</textarea>
       </div>
-
       <hr>
     </div>
   </div>
@@ -88,37 +86,43 @@ td {
   border: none;
 }
 .red {
-  background-color: #ff0000;
+  background-color: white;
   border-radius: 0.25em;
-  border: 1px solid #ff0000;
-  color: #ffffff;
-  font-size: 0.8em;
-  padding: 0.5em;
+  border: none;
+  padding: 7px 28px;
 }
-.gray {
-  background-color: #585a5c;
-  border-radius: 0.25em;
-  border: 1px solid #585a5c;
-  color: #ffffff;
-  font-size: 0.8em;
-  padding: 0.5em;
-  
-}
-.green {
-  background-color: #05cc47;
-  border-radius: 0.25em;
-  border: 1px solid #05cc47;
-  color: #ffffff;
-  font-size: 0.8em;
-  padding: 0.5em;
+.red:hover {
+  background-color: red;
+  color: white;
 }
 
+.gray {
+  background-color: white;
+  border-radius: 0.25em;
+  border: none;
+  padding: 7px 28px;
+}
+.gray:hover {
+  background-color: #ff9900;
+  color: white;
+}
+.green {
+  background-color: white;
+  border-radius: 0.25em;
+  border: none;
+  padding: 7px 28px;
+}
+.green:hover {
+  background-color: #00ad45;
+  color: white;
+}
 input[type="number"],
 input[type="text"],
 textarea {
-  border: 1px solid #e0e0e0;
+  border: 2px solid #e0e0e0;
   padding: 0.85em 0.75em 0.75em 0.75em;
   width: calc(100% - 1.25em);
   resize: none;
 }
 </style>
+
